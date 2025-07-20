0cd3c392f85edcf9d7a127a499609c2a98a116d0
Soumaydeep Chakravarti <soumyadeepsai1@gmail.com>
2025-07-20 04:31:11 +0000

    made code modular again

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,15 +4,7 @@ import { getAuth } from 'firebase/auth'; // Only import getAuth here
 // import { getFirestore } from 'firebase/firestore'; // Uncomment if you need Firestore
 // import { getAnalytics } from "firebase/analytics"; // Uncomment if you need Analytics
 
-const FirebaseConfigKeys = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
+import FirebaseConfigKeys from '../config/FirebaseConfigKeys'; // Import the configuration object
 
 const app = initializeApp(FirebaseConfigKeys);
 
